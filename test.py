688c83b5a9315f131366256fb8a9dba061b85723
AndrewZhou924 <andrewzhou924@gmail.com>
2020-07-27 17:00:23 +0800

    init repo

---
+++ test.py
@@ -0,0 +1,40 @@
+import torch, torchvision
+print(torch.__version__, torch.cuda.is_available())
+
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+# from google.colab.patches import cv2_imshow
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+# !wget http://images.cocodataset.org/val2017/000000439715.jpg -q -O input.jpg
+
+im = cv2.imread("./input.jpg")
+# cv2_imshow(im)
+
+'''
+    Then, we create a detectron2 config and a detectron2 DefaultPredictor to run inference on this image.
+'''
+cfg = get_cfg()
+# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+
+print(outputs["instances"].pred_classes)
+print(outputs["instances"].pred_boxes)
